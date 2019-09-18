f6f0ebabc7c7707e5e9e87ef26e2bc3c9d752998
Nathan Mella <nathanmell@gmail.com>
2019-09-18 08:18:52 -0600

    Adjusted iteration method so that I can run smaller jobs

--- violin_cancer_type.R
+++ violin_cancer_type.R
@@ -6,11 +6,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 df2 <- read_tsv(args[1]) %>%
   mutate(CancerType = str_replace(CancerType, "^TCGA_", ""), Algorithm=factor(Algorithm)) %>%
-  group_by(Algorithm, CancerType, Description, Iteration) %>% summarize(AUROC=mean(AUROC)) %>%
-  group_by(Algorithm, CancerType, Description) %>%
-  summarize(AUROC=mean(AUROC))
+  group_by(Algorithm, CancerType, Description, Iteration) %>% summarize(AUROC=mean(AUROC))
 
-df2 <- df2 %>% group_by(CancerType, Description) %>% summarize(AUROC_max = max(AUROC), AUROC_min = min(AUROC), AUROC=mean(AUROC)) #%>% inner_join(df2, by = c("AUROC", "CancerType", "Description"))
 
 # df3 <- df2 %>% group_by(CancerType, Description) %>% summarize(AUROC = max(AUROC)) %>% inner_join(df2, by = c("AUROC", "CancerType", "Description"))
 
@@ -21,7 +18,7 @@ df2 %>%
   ggplot(aes(x = Description, y = AUROC, color = Description, fill = Description)) +
   theme_bw() +
   scale_shape_manual(values=c(3,4,7,8,11,15,17,18,19,6)) +
-  geom_linerange(ymin = df2$AUROC_min, ymax = df2$AUROC_max) +
+  geom_violin() +
   coord_flip() +
   geom_hline(yintercept = 0.5, color = "red", linetype = "dashed") +
   facet_grid(rows = vars(CancerType)) +
@@ -29,6 +26,6 @@ df2 %>%
   ylim(0.40, 0.78) +
   theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
 
-ggsave(graph_name, height = 7, width = 10, dpi = 300)
+ggsave(graph_name, height = 10, width = 7, dpi = 300)
 
 
